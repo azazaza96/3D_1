7bd49cbdd5157a329530e7f46394c61004e67b90
安住　瑠唯 <SDT2395GP0001@edu.sca.ac.jp>
2023-05-19 12:29:46 +0900

    Unity 追尾のスクリプト

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //毎フレーム力を加える
+        rb.AddForce(player.transform.position-transform.position);
+        //何もついてないtransform.positionは、アタッチされている
+        //物体の座標になる
+    }
+}
